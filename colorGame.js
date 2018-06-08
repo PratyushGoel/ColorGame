54f26545c882882a0814230d5b00bc895b10342a
Pratyush Goel <pratyushgoel99@gmail.com>
2018-06-08 18:10:41 +0530

    Setup html and css files and basic js

--- colorGame.js
+++ colorGame.js
@@ -1,5 +1,27 @@
-var colors = ["50,0,0", "0,50,0", "0,0,50", "50,50,0", "0,50,50", "50,0,50"] ;
-
+var colors = ["red", "yellow", "blue", "green", "orange", "pink"] ;
 var boxes = document.querySelectorAll(".boxes");
+var realColor = colors[Math.floor(Math.random()*7)];
+var i;
 
-=
+
+
+
+
+//Providing color to boxes
+for(i=0; i<6; i++){
+	boxes[i].style.background = colors[i] ;
+}
+
+//Clicking on boxes
+for(i=0; i<6; i++){
+	boxes[i].addEventListener('click', function(){
+		//if it is real color
+		if(this.style.background === realColor){
+			console.log("You picked right Color");
+		}
+		
+	})
+}
+
+
+
