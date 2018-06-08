b8e7061714d7c52b597e4bbdfc8ade2e0c8eb5c8
Pratyush Goel <pratyushgoel99@gmail.com>
2018-06-08 18:45:21 +0530

    Add functioning of box click event

--- colorGame.js
+++ colorGame.js
@@ -1,7 +1,11 @@
 var colors = ["red", "yellow", "blue", "green", "orange", "pink"] ;
 var boxes = document.querySelectorAll(".boxes");
+var heading = document.querySelector("h1");
+var message = document.querySelector("#message");
 var realColor = colors[Math.floor(Math.random()*7)];
 var i;
+
+
 
 
 
@@ -18,6 +22,15 @@ for(i=0; i<6; i++){
 		//if it is real color
 		if(this.style.background === realColor){
 			console.log("You picked right Color");
+			heading.style.background = realColor;
+			message.textContent = "Correct";
+			for(i=0; i<6; i++){
+				boxes[i].style.background = realColor;
+			}
+		}
+		else{
+			this.style.background = "#232323";
+			message.textContent = "Try Again";
 		}
 		
 	})
