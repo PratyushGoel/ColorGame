8167704d3ee09358e2e71af1708e9b0ae06f27b0
Pratyush Goel <pratyushgoel99@gmail.com>
2018-06-09 12:19:59 +0530

    Add Mode Buttons

--- colorGame.js
+++ colorGame.js
@@ -1,25 +1,36 @@
 var colors = []; 
 var boxes = document.querySelectorAll(".boxes");
+var mode = document.querySelectorAll(".mode");
 var heading = document.querySelector("h1");
 var message = document.querySelector("#message");
 var reset = document.querySelector("#reset");
 var colorDisplay = document.querySelector("#colorDisplay");
 var realColor;
 var i;
+var n=6;
 
 setBoxes();
 
+for(i=0; i<2; i++){
+	mode[i].addEventListener('click', function(){
+		mode[0].classList.remove("selected");
+		mode[1].classList.remove("selected");
+		this.classList.add("selected");
+		this.textContent === "Easy" ? n = 3: n = 6;
+		setBoxes();
+	})
+}
 
 
 //Clicking on boxes
-for(i=0; i<6; i++){
+for(i=0; i<n; i++){
 	boxes[i].addEventListener('click', function(){
 		//if it is real color
 		if(this.style.background === realColor){
 			heading.style.background = realColor;
 			message.textContent = "Correct";
 			reset.textContent = "Play Again?";
-			for(i=0; i<6; i++){
+			for(i=0; i<n; i++){
 				boxes[i].style.background = realColor;
 			}
 		}
@@ -41,15 +52,22 @@ reset.addEventListener('click', function(){
 
 
 function setBoxes(){
-	for(i=0; i<6; i++){
-		colors.push(setColor());
+	colors = [];
+	for(i=0; i<n; i++){
+		colors[i] = setColor();
 	}
-	realColor = colors[Math.floor(Math.random()*6)];
+	realColor = colors[Math.floor(Math.random()*n)];
 	colorDisplay.textContent = realColor;
 
 	//Providing color to boxes
 	for(i=0; i<6; i++){
-		boxes[i].style.background = colors[i] ;
+		if(colors[i]){
+			boxes[i].style.display = "block";
+			boxes[i].style.background = colors[i] ;
+		} 
+		else{
+			boxes[i].style.display = "none";
+		}
 	}
 }
 
